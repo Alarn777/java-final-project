21e64013a234ab10095a738fa92b5b4e2e938a1a
Evgeny Alterman <sworgkh@gmail.com>
2019-02-25 17:50:35 +0200

    Refactorings and Offline File

--- src/CurrencyServiceRepository.java
+++ src/CurrencyServiceRepository.java
@@ -1,115 +1,132 @@
 
-import java.io.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
-import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.Vector;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
 
 public class CurrencyServiceRepository {
     static Logger log = LogManager.getLogger(CurrencyServiceRepository.class.getName());
 
     private final String lastDownloadedRatesFilePath = "lastCurrencyCheck.xml";
-    private Vector returnArr;
+    private final String serviceUrl = "https://www.boi.org.il/currency.xml";
+
     private FileTime lastSyncDate;
 
     public CurrencyServiceRepository() {
         getLastSyncDate();
     }
 
-    private void getLastSyncDate(){
-
+    private void getLastSyncDate() {
         try {
-            this.lastSyncDate = Files.getLastModifiedTime(Paths.get(lastDownloadedRatesFilePath));
+            lastSyncDate = Files.getLastModifiedTime(Paths.get(lastDownloadedRatesFilePath));
         } catch (IOException e) {
             log.error("Can't read currencies file");
         }
     }
+
+
     public Vector getCurrData() {
         try {
+            var lastUpdateTimestamp = lastSyncDate.toMillis();
+
             DownloadLatestRates();
 
-            InputStream savedFileStream = new FileInputStream(
-                    new File(lastDownloadedRatesFilePath));
+            if (lastUpdateTimestamp == lastSyncDate.toMillis())
+                log.info("Using previously downloaded rates from "
+                        + new SimpleDateFormat("dd/MM/yyyy - hh:mm:ss").format(lastSyncDate.toMillis())
+                );
 
+            return parseRatesXmlFile();
 
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(savedFileStream);
-            doc.getDocumentElement().normalize();
-            NodeList nList = doc.getElementsByTagName("CURRENCY");
-            returnArr = new Vector();
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-                Node nNode = nList.item(temp);
-                Vector oneLine = new Vector();
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-
-                    String value = eElement
-                            .getElementsByTagName("NAME")
-                            .item(0)
-                            .getTextContent();
-
-
-                    oneLine.add(value);
-
-                    value = eElement
-                            .getElementsByTagName("CURRENCYCODE")
-                            .item(0)
-                            .getTextContent();
-
-
-                    oneLine.add(value);
-
-                    value = eElement
-                            .getElementsByTagName("COUNTRY")
-                            .item(0)
-                            .getTextContent();
-
-
-                    oneLine.add(value);
-
-                    value = eElement
-                            .getElementsByTagName("RATE")
-                            .item(0)
-                            .getTextContent();
-
-                    oneLine.add(value);
-
-                    value = eElement
-                            .getElementsByTagName("UNIT")
-                            .item(0)
-                            .getTextContent();
-
-                    oneLine.add(value);
-
-
-                    returnArr.add(oneLine);
-                }
-            }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Error on loading rates.");
+            log.error(e.getStackTrace());
+            return null;
         }
+    }
 
-        return returnArr;
+    private Vector parseRatesXmlFile() throws ParserConfigurationException, SAXException, IOException {
+        var savedFileStream = new FileInputStream(new File(lastDownloadedRatesFilePath));
+
+        var factory = DocumentBuilderFactory.newInstance();
+        var builder = factory.newDocumentBuilder();
+        var doc = builder.parse(savedFileStream);
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("CURRENCY");
+
+        var rates = new Vector();
+        for (int i = 0; i < nList.getLength(); i++) {
+
+            Node nNode = nList.item(i);
+            Vector oneLine = new Vector();
+
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element elm = (Element) nNode;
+                String value = elm
+                        .getElementsByTagName("NAME")
+                        .item(0)
+                        .getTextContent();
+
+
+                oneLine.add(value);
+
+                value = elm
+                        .getElementsByTagName("CURRENCYCODE")
+                        .item(0)
+                        .getTextContent();
+
+
+                oneLine.add(value);
+
+                value = elm
+                        .getElementsByTagName("COUNTRY")
+                        .item(0)
+                        .getTextContent();
+
+
+                oneLine.add(value);
+
+                value = elm
+                        .getElementsByTagName("RATE")
+                        .item(0)
+                        .getTextContent();
+
+                oneLine.add(value);
+
+                value = elm
+                        .getElementsByTagName("UNIT")
+                        .item(0)
+                        .getTextContent();
+
+                oneLine.add(value);
+
+                rates.add(oneLine);
+            }
+        }
+        return rates;
     }
 
     private void DownloadLatestRates() throws IOException {
         try {
             log.info("Starting rates download.");
 
-            URL url = new URL("https://www.boi.org.il/currency.xml");
+            URL url = new URL(serviceUrl);
             var connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
@@ -122,6 +139,7 @@ public class CurrencyServiceRepository {
             new FileOutputStream(targetFile).write(buffer);
 
             getLastSyncDate();
+            log.info("Downloading rates from " + serviceUrl + " completed.");
 
         } catch (Exception e) {
             log.error("Failed to download latest currency rates.");
